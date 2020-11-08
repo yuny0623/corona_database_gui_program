ac4fb5d710dfbf3537ccc5d59d86b70012ac56c1
yuny0623 <65948937+yuny0623@users.noreply.github.com>
2020-11-08 16:11:01 +0900

    끝났당ㅋㅋ
    보완해야할점.
    1. 이미지가 경로가 안먹힌다 왜그러지? 절대경로는 잘 먹히는데, 상대경로를 인식을 못한다. 정석대로 잘 썼는데, 왜 인식을 못할까? 
    2. MYSQL 3306에 포워딩을 해줘야 외부 컴퓨터에서 사용할 수 있는데, 아직 포워딩 안해줬다. 그래서 지금은 서버컴퓨터에서만 사용가능함. 
    
    전반적으로 평가해봤을때.... 
    음... 잘만들어진듯. 약간 투박한 UI에 조금 부족한 부분도 있는데, 그래도 뭐

--- corona/src/main_act/corona_main.java
+++ corona/src/main_act/corona_main.java
@@ -2,24 +2,20 @@
 package main_act;
 import java.sql.Connection;
 public class corona_main {
-
+	
+	
+	/*
+	 * 코로나 데이터베이스 GUI프로그램입니다. 
+		1. mysql 서버가 켜져있어야 사용할 수 있습니다. 
+		2. mysql 3306 포워딩이 되어있어야 외부에서 사용할 수 있습니다. 
+		3. 1번과 2번이 충족되지 않으면 서버컴퓨터에서만 사용할 수 있습니다. 외부에서는 사용불가 
+		4. 아이디와 비밀번호는 각각 root와 password 입니다. 
+		5. 프로그램에 사용된 데이터베이스는 최신데이터가 아닙니다. 현재와는 다를 수 있습니다. 
+	 */
+	
+	
 	public static void main(String[] args) {
-		
-		
 		new login_menu(); //로그인 메뉴창 생성하기./ 여기서 통과해야. 즉 아이디랑 비번이 맞아야 프로그램이 실행된다. 
-
-
 	}
 }
 
-//if (s.var == 1) {
-//System.out.println(s.var);
-//System.out.println("ok to go");
-//new guiAct2(); //gui창을 생성하는 클래스의 생성자입니다. 
-//}	
-
-
-
-//일반 이용자 모드 사용할 건지. 아니면, 관리자 이용할 건지 선택하는 창 뜨게 하기. 
-//관리자 모드 추가하기. 로그인 창 입력해서. mysql login id랑 password 입력하고, 입력 성공하면 관리자 창 열어주기. 
-//관리자 창에서는 sql문 직접 전달할 수 있음. 
