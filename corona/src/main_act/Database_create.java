07e035ca6936212642a9a773da1c0312448fc692
yuny0623 <65948937+yuny0623@users.noreply.github.com>
2020-11-11 15:53:16 +0900

    add JPasswordField and add menubar text

--- corona/src/main_act/Database_create.java
+++ corona/src/main_act/Database_create.java
@@ -5,8 +5,8 @@ package main_act;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Database_create {
